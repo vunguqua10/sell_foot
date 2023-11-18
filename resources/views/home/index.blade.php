f958a812944b4af6efcd03b195ea3d3c4963ded1
honghao1408 <nhh14082002@gmail.com>
2023-11-18 14:46:17 +0700

    up

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -120,7 +120,7 @@
 
                                     <li><a href="{{route('show_detail',$product->id)}}" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
                                     <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
-                                    <li><a href="{{ route('add-to-wishlist',$product->id) }}" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
+                                    <li><a href="{{ route('add-to-wishlist',$product->id) }} " data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li>
 
                                 </ul>
                                 <a class="cart" href="{{ route('add-to-cart',$product->id) }}">Add to Cart</a>
